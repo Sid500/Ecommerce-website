1dffacf08eac8f5179319f44144430778b910a8c
Sid500 <72332611+Sid500@users.noreply.github.com>
2022-03-27 15:33:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,3 +82,12 @@ var swiper = new Swiper(".review-slider", {
       },
     },
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
